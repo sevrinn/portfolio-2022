656b303609a01bab45e8e85414994a1e876dc445
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-21 11:20:17 -0800

    mobile first header

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
 const Logo = () => {
-  return (
-    <div id='logo' className='half-width'>
-      <img
-        src={require('../images/pineapple-multi-color.png')}
-        alt='Pineapple logo'
-        id='logo-icon'
-      />
-      <h1>
-        <span className='sev'>Sev</span>
-        <span className='welker'>Welker</span>
-      </h1>
-    </div>
-  )
+	return (
+		<div id='logo'>
+			<img
+				src={require('../images/pineapple-multi-color.png')}
+				alt='Pineapple logo'
+				id='logo-icon'
+			/>
+			<h1>
+				<span className='sev'>Sev</span>
+				<span className='welker'>Welker</span>
+			</h1>
+		</div>
+	)
 }
 
 export default Logo
