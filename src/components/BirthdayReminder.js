1f3e44ad3962c0b57e6e95fb164d2fffa7d252d7
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-22 09:08:46 -0800

    added birthday reminder component and data

---
+++ src/components/BirthdayReminder.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+import birthdayData from '../birthdayData'
+
+const BirthdayReminder = () => {
+  return <h1>birthday reminder</h1>
+}
+
+export default BirthdayReminder
