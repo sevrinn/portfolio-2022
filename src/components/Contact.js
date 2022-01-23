bd3f7c6dfbfc6e97cb70ce547a5c2bf4e2494a22
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-23 09:55:10 -0800

    added contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,45 @@
 import React from 'react'
 
 const Contact = () => {
-	return (
-		<div>
-			<h1 className='contact-title'>contact</h1>
-		</div>
-	)
+  return (
+    <div className='contact-container'>
+      <h1 className='contact-title'>contact</h1>
+      <div className='contact-form-container'>
+        <p>
+          Want to hire me? Have a question? Leave a message here and I'll get
+          back to you in a timely manner:)
+        </p>
+        <form
+          name='contact'
+          method='POST'
+          data-netlify='true'
+          className='contact-form'
+        >
+          <p>
+            <input
+              type='text'
+              name='name'
+              id='name'
+              placeholder='name'
+              pattern='[A-z\sa-z]{3,20}'
+            />
+          </p>
+          <p>
+            <input
+              type='email'
+              name='email'
+              placeholder='email'
+              pattern='[A-z\sa-z]{3,20}'
+            />
+          </p>
+          <p>
+            <textarea name='message' placeholder='message'></textarea>
+          </p>
+          <button type='submit'>Send</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
