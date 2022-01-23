a7f86bd47ecfbe9f99d44fec5978679e720032ee
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-22 20:47:08 -0800

    mapped projects data successfully

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import projectData from '../projectData'
 
 const Projects = () => {
-	return (
-		<div>
-			<h1 className='project-title'>projects</h1>
-		</div>
-	)
+  const [projects, setProjects] = useState(projectData)
+  console.log(projects)
+  return (
+    <div>
+      <h1 className='project-title'>projects</h1>
+      <div className='projects-container'>
+        {projects.map((project) => {
+          const { id, title, img, desc } = project
+          return (
+            <article key={id} className='project'>
+              <img src={img} alt={title} />
+              <div className='project-copy'>
+                <h4>{title}</h4>
+                <p>{desc}</p>
+              </div>
+            </article>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Projects
