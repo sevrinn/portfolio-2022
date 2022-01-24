fe694903e78399381b136b93c64b2fd708101b64
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-23 20:50:15 -0800

    added dummy values

---
+++ src/projectData.js
@@ -0,0 +1,40 @@
+const projects = [
+  {
+    id: 1,
+    title: 'Birthday Reminder',
+    img: require('./images/project-img/birthday-reminder.JPG'),
+    desc: 'A birthday reminder componenet that maps thru a set of data and displays it',
+  },
+  {
+    id: 2,
+    title: 'Shmirthday Rescinder',
+    img: require('./images/project-img/birthday-reminder.JPG'),
+    desc: 'A birthday reminder componenet that maps thru a set of data and displays it',
+  },
+  {
+    id: 3,
+    title: 'Death Defier',
+    img: require('./images/project-img/birthday-reminder.JPG'),
+    desc: 'A birthday reminder componenet that maps thru a set of data and displays it',
+  },
+  {
+    id: 4,
+    title: 'Birthday Reminder',
+    img: require('./images/project-img/birthday-reminder.JPG'),
+    desc: 'A birthday reminder componenet that maps thru a set of data and displays it',
+  },
+  {
+    id: 5,
+    title: 'Shmirthday Rescinder',
+    img: require('./images/project-img/birthday-reminder.JPG'),
+    desc: 'A birthday reminder componenet that maps thru a set of data and displays it',
+  },
+  {
+    id: 6,
+    title: 'Death Defier',
+    img: require('./images/project-img/birthday-reminder.JPG'),
+    desc: 'A birthday reminder componenet that maps thru a set of data and displays it',
+  },
+]
+
+export default projects
