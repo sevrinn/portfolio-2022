a7593b6bce157109761f2c2af8e373a00b0a4b92
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-24 17:28:44 -0800

    update data file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,44 +5,40 @@ import { VscFileCode } from 'react-icons/vsc'
 import projectData from '../projectData'
 
 const Projects = () => {
-  const [projects, setProjects] = useState(projectData)
-  console.log(projects)
-  return (
-    <div id='projects'>
-      <h1 className='project-title'>projects</h1>
-      <div className='projects-container'>
-        {projects.map((project) => {
-          const { id, url, title, img, desc } = project
-          return (
-            <article key={id} className='project'>
-              <img src={img} alt={title} />
-              <div className='project-copy'>
-                <h4>
-                  <Link className='Link' to={url}>
-                    {title}
-                  </Link>
-                </h4>
+	const [projects, setProjects] = useState(projectData)
+	console.log(projects)
+	return (
+		<div id='projects'>
+			<h1 className='project-title'>projects</h1>
+			<div className='projects-container'>
+				{projects.map((project) => {
+					const { id, url, github, title, img, desc } = project
+					return (
+						<article key={id} className='project'>
+							<img src={img} alt={title} />
+							<div className='project-copy'>
+								<h4>
+									<a href={url} target='_blank' rel='noreferrer'>
+										{title}
+									</a>
+								</h4>
 
-                <p>{desc}</p>
-                <div className='proj-links'>
-                  <Link className='Link' to={url}>
-                    <MdComputer size={30} />
-                  </Link>
-                  <a
-                    href='https://github.com/sevrinn/portfolio-2022/tree/main/src/projects'
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    <VscFileCode size={30} />
-                  </a>
-                </div>
-              </div>
-            </article>
-          )
-        })}
-      </div>
-    </div>
-  )
+								<p>{desc}</p>
+								<div className='proj-links'>
+									<Link className='Link' to={url}>
+										<MdComputer size={30} />
+									</Link>
+									<a href={github} target='_blank' rel='noreferrer'>
+										<VscFileCode size={30} />
+									</a>
+								</div>
+							</div>
+						</article>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
 
 export default Projects
